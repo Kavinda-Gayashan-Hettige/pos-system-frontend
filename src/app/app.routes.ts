46442a4b63ad781b142ce4e2c9e420f655c9aba9
kavinda1996 <gayashankavinda95@gmail.com>
2026-06-11 13:11:19 +0530

    create nav-bar with using routerOutlet

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { Home } from './pages/home/home';
+import { Products } from './pages/products/products';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: 'home', component: Home}, 
+    {path: 'products', component: Products},
+];
